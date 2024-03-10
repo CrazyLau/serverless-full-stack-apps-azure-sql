7256347970a1b1db409f1f458edd9cf072c8feba
Louisa <louisa.ahlenius@volvo.com>
2024-03-10 15:00:27 +0100

    oklart

--- azure-static-web-app/client/src/App.jsx
+++ azure-static-web-app/client/src/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import RecipeTitle from './RecipeTitle'
+import TableComponent from './TableComponent'
 import './index.css'
 
 function callApi() {
     fetch('https://proud-bush-0cec8fb03.5.azurestaticapps.net/api/bus-data') // api for the get request
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then((data) => {return 'hej';})
+    // return data
+    // return [{"Id": 1, "Item": "Apples", "Amount": 2}]
   }
-  
+
+function myFunction() {
+document.getElementById("demo").innerHTML = callApi();
+}
 
 function App() {
     return (
-        <button onClick={callApi}>Call API</button>
+        <div className="article">
+                <h2>Hello World!</h2> 
+                <TableComponent data={callApi()} />
+
+                <button onClick={myFunction}>Fetch data</button>
+                <p id="demo"></p>
+
+                <button onClick={myFunction}>Run Databricks job</button>
+ 
+        </div>
     )
 }
 
