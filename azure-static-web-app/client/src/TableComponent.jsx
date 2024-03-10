97082de0bd9731136dc7aebb7b7f7cf51a68fe3d
Louisa <louisa.ahlenius@volvo.com>
2024-03-10 20:31:10 +0100

    Refactored TableComponents

--- azure-static-web-app/client/src/TableComponent.jsx
+++ azure-static-web-app/client/src/TableComponent.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const TableComponent = ({ data }) => {
   if (!data || data.length === 0) {
-    return <div>No data available</div>; // Display a message if data is null, undefined, or empty
+    return <div>No data available</div>;
   }
 
-  const headers = Object.keys(data[0] || {}); // Use the keys of the first object to determine headers
+  const headers = Object.keys(data[0] || {});
 
   return (
     <table>
@@ -15,9 +15,9 @@ const TableComponent = ({ data }) => {
         </tr>
       </thead>
       <tbody>
-        {data.map((row, index) => (
-          <tr key={index}>
-            {headers.map((header, index) => <td key={index}>{row[header]}</td>)}
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {headers.map((header, cellIndex) => <td key={cellIndex}>{row[header]}</td>)}
           </tr>
         ))}
       </tbody>
