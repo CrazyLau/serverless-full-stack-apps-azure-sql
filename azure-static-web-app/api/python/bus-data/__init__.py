aca08d0e9bf3683cf367c077012767ca3201e285
Louisa <louisa.ahlenius@volvo.com>
2024-03-10 18:43:50 +0100

    try to fetch all rows in api

--- azure-static-web-app/api/python/bus-data/__init__.py
+++ azure-static-web-app/api/python/bus-data/__init__.py
@@ -9,15 +9,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try: 
         with pyodbc.connect(AZURE_CONN_STRING) as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM dbo.GroceryList FOR JSON AUTO, INCLUDE_NULL_VALUES")
+            cursor.execute("SELECT * FROM dbo.GroceryList FOR JSON AUTO, INCLUDE_NULL_VALUES, WITHOUT_ARRAY_WRAPPER")
             rows = cursor.fetchall()
 
             # Initialize an empty list to store JSON objects for each row
-            result = []
-            
+            # result = []
+            result = rows
             # Iterate through each row and append its JSON representation to the result list
-            for row in rows:
-                result.append(json.loads(row[0]))
+            # for row in rows:
+            #     result.append(json.loads(row[0]))
             
     except pyodbc.Error as e:
         error_string = f"Database error: {str(e)}"
@@ -27,4 +27,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         error_string = f"Error: {str(e)}"
         return func.HttpResponse(error_string, status_code=500)
         
-    return func.HttpResponse(json.dumps(result), status_code=200)
+    return func.HttpResponse(result, status_code=200)  # json.dumps(result)
