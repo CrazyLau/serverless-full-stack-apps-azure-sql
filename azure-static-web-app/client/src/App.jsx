80f21e6d8e23f8065fd400fa372158410e2a543a
Louisa <louisa.ahlenius@volvo.com>
2024-03-10 16:53:23 +0100

    changed api path to my own api

--- azure-static-web-app/client/src/App.jsx
+++ azure-static-web-app/client/src/App.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import TableComponent from './TableComponent';
 import './index.css';
 
+
 function callApi() {
-    return fetch('https://dummyjson.com/products/1')
+    return fetch('https://proud-bush-0cec8fb03.5.azurestaticapps.net/api/bus-data') // For testing: 'https://dummyjson.com/products/1' 
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to fetch data');
